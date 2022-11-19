203111bf579f65981e76b5ba6da660de0e65344d
Serenthony <116910131+Serenthony@users.noreply.github.com>
2022-11-18 22:34:54 -0600

    Coomit

--- app/src/main/java/com/example/proyecto_tarjeta/Loading.java
+++ app/src/main/java/com/example/proyecto_tarjeta/Loading.java
@@ -2,7 +2,9 @@ package com.example.proyecto_tarjeta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Loading extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class Loading extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+        final int duracion = 3000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Loading.this, getClass());
+                startActivity(intent);
+                finish();
+            }
+        }, duracion);
     }
 }
--- app/src/main/java/com/example/proyecto_tarjeta/Registro.java
+++ app/src/main/java/com/example/proyecto_tarjeta/Registro.java
@@ -1,5 +1,6 @@
 package com.example.proyecto_tarjeta;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
@@ -12,6 +13,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
@@ -20,18 +34,26 @@ public class Registro extends AppCompatActivity {
     private boolean passwordShowing = false;
     private boolean Check = false;
 
+    EditText nombre, apellidos, telefono, correo, contrasena, nombre_usuario;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
+        nombre = findViewById(R.id.txt_name);
+        apellidos = findViewById(R.id.txt_lastname);
+        telefono = findViewById(R.id.txt_telephone);
+        correo = findViewById(R.id.txt_email);
+        contrasena = findViewById(R.id.txt_password);
+        nombre_usuario = findViewById(R.id.txt_username);
+
         Typeface typeface = ResourcesCompat.getFont(getApplicationContext(), R.font.gilroy_medium);
         final Button sign_up = findViewById(R.id.btnSignUp);
         final TextView signIn = findViewById(R.id.txtsignIn);
-        final EditText password = findViewById(R.id.txt_password);
-        final EditText email = findViewById(R.id.txt_email);
         final ImageView check_button = findViewById(R.id.unchecked_button);
         final ImageView passwordIcon = findViewById(R.id.passwordIcon);
+
 
         check_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,19 +75,19 @@ public class Registro extends AppCompatActivity {
                 //Verificamos si la contraseña se está mostrando o no
                 if (passwordShowing){
                     passwordShowing = false;
-                    password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    contrasena.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
                     passwordIcon.setImageResource(R.drawable.password_show);
                     //Tomando y asignando el tipo de letra al input
-                    password.setTypeface(typeface);
+                    contrasena.setTypeface(typeface);
                 }else{
                     passwordShowing = true;
-                    password.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    contrasena.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
                     passwordIcon.setImageResource(R.drawable.password_hide);
                     //Tomando y asignando el tipo de letra al input
-                    password.setTypeface(typeface);
+                    contrasena.setTypeface(typeface);
                 }
                 //Mueve el cursor a la última parte del texto
-                password.setSelection(password.length());
+                contrasena.setSelection(contrasena.length());
             }
         });
 
@@ -73,7 +95,25 @@ public class Registro extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                final String getEmailTxt = email.getText().toString();
+                ejecutarServicio("https://nizi.red-utz.com/insertar_usuario.php");
+
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Registro.this, Login.class));
+            }
+        });
+    }
+
+    private void ejecutarServicio (String URL){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                final String getEmailTxt = correo.getText().toString();
 
                 new SweetAlertDialog(Registro.this, SweetAlertDialog.SUCCESS_TYPE)
                         .setTitleText("Cuenta creada")
@@ -89,15 +129,30 @@ public class Registro extends AppCompatActivity {
                             }
                         })
                         .show();
-            }
-        });
 
-        signIn.setOnClickListener(new View.OnClickListener() {
+
+
+            }
+        }, new Response.ErrorListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Registro.this, Login.class));
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "No se logró", Toast.LENGTH_SHORT).show();
             }
-        });
-
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("nombre", nombre.getText().toString());
+                parametros.put("apellidos", apellidos.getText().toString());
+                parametros.put("correo", correo.getText().toString());
+                parametros.put("telefono", telefono.getText().toString());
+                parametros.put("username", nombre_usuario.getText().toString());
+                parametros.put("contrasena", contrasena.getText().toString());
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
