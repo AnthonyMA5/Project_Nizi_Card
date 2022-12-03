529b96e5247f17c3708d810cfd8ca1df6db4bd7f
Serenthony <116910131+Serenthony@users.noreply.github.com>
2022-12-03 00:37:26 -0600

    Coomit

--- app/src/main/java/com/example/proyecto_tarjeta/Domicilio.java
+++ app/src/main/java/com/example/proyecto_tarjeta/Domicilio.java
@@ -1,16 +1,40 @@
 package com.example.proyecto_tarjeta;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class Domicilio extends AppCompatActivity {
 
     String idU;
+    Button actDir;
     ImageView regresar;
+    EditText calle, num_ex, num_in, cp, colonia, ciudad, estado, pais;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +42,24 @@ public class Domicilio extends AppCompatActivity {
         setContentView(R.layout.activity_domicilio);
 
         idU = getIntent().getStringExtra("idU");
+        direccionUsuario("https://nizi.red-utz.com/informacion_usuario.php?idU="+idU+"");
         regresar = findViewById(R.id.btnAtrasAddress);
+        calle = findViewById(R.id.calle_address);
+        num_ex = findViewById(R.id.numext_address);
+        num_in = findViewById(R.id.numint_address);
+        cp = findViewById(R.id.cp_address);
+        colonia = findViewById(R.id.colonia_address);
+        ciudad = findViewById(R.id.ciudad_address);
+        estado = findViewById(R.id.estado_address);
+        pais = findViewById(R.id.pais_address);
+        actDir = findViewById(R.id.act_direccion);
+
+        actDir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actualizarDireccion("https://nizi.red-utz.com/actualizar_direccion_usuario.php");
+            }
+        });
 
         regresar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +69,69 @@ public class Domicilio extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void actualizarDireccion(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new com.android.volley.Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                new SweetAlertDialog(Domicilio.this, SweetAlertDialog.SUCCESS_TYPE)
+                        .setTitleText("Dirección actualizada correctamente")
+                        .show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<>();
+                parametros.put("idU", idU);
+                parametros.put("municipio", ciudad.getText().toString());
+                parametros.put("estado", estado.getText().toString());
+                parametros.put("cp", cp.getText().toString());
+                parametros.put("colonia", colonia.getText().toString());
+                parametros.put("num_ext", num_ex.getText().toString());
+                parametros.put("num_int", num_in.getText().toString());
+                parametros.put("calle", calle.getText().toString());
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private void direccionUsuario(String URL) {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(URL, new com.android.volley.Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                JSONObject jsonObject = null;
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        calle.setText(jsonObject.getString("calle"));
+                        num_ex.setText(jsonObject.getString("num_ext"));
+                        num_in.setText(jsonObject.getString("num_int"));
+                        cp.setText(jsonObject.getString("c_p_codigo"));
+                        colonia.setText(jsonObject.getString("colonia"));
+                        //ciudad.setText(jsonObject.getString("calle"));
+                        //estado.setText(jsonObject.getString("calle"));
+                        //pais.setText(jsonObject.getString("calle"));
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error de conexión", Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
     }
 }
--- app/src/main/java/com/example/proyecto_tarjeta/Tarjeta.java
+++ app/src/main/java/com/example/proyecto_tarjeta/Tarjeta.java
@@ -1,6 +1,7 @@
 package com.example.proyecto_tarjeta;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,13 +10,30 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Tarjeta extends AppCompatActivity {
 
     String idU;
+    TextView t_num, t_fecha;
+    TextView nombre, numero, vigencia, cvv;
     BottomNavigationView nav_bottom;
     ImageView regresar;
 
@@ -25,8 +43,17 @@ public class Tarjeta extends AppCompatActivity {
         setContentView(R.layout.activity_tarjeta);
 
         idU = getIntent().getStringExtra("idU");
+        datosTarjeta("https://nizi.red-utz.com/info_tarjeta_usuario.php?idU="+idU+"");
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        t_num = findViewById(R.id.numTarjeta);
+        t_fecha = findViewById(R.id.vigenciaTarjeta);
+
+        nombre = findViewById(R.id.tarjeta_dueño_tarjeta);
+        numero = findViewById(R.id.tarjeta_num_tarjeta);
+        vigencia = findViewById(R.id.tarjeta_vigencia_tarjeta);
+        cvv = findViewById(R.id.tarjeta_cvv_tarjeta);
 
         nav_bottom = findViewById(R.id.bttom_nav);
         regresar = findViewById(R.id.regresar_home_tarjeta);
@@ -71,5 +98,34 @@ public class Tarjeta extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void datosTarjeta(String URL) {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                JSONObject jsonObject = null;
+                for (int i = 0; i < response.length(); i++){
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        t_num.setText(jsonObject.getString("num1")+" "+jsonObject.getString("num2")+" "+jsonObject.getString("num3")+" "+jsonObject.getString("num4"));
+                        t_fecha.setText(jsonObject.getString("vigencia"));
+                        nombre.setText(jsonObject.getString("nombre")+" "+jsonObject.getString("apellido"));
+                        numero.setText(jsonObject.getString("num1")+" "+jsonObject.getString("num2")+" "+jsonObject.getString("num3")+" "+jsonObject.getString("num4"));
+                        vigencia.setText(jsonObject.getString("vigencia"));
+                        cvv.setText(jsonObject.getString("cuv"));
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error de conexión", Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
     }
 }
