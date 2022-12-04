8f0dbf2bbc49ef291fb441f143142568e641bbc8
Serenthony <116910131+Serenthony@users.noreply.github.com>
2022-12-04 01:45:58 -0600

    Coomit

--- app/src/main/java/com/example/proyecto_tarjeta/Forget_Password.java
+++ app/src/main/java/com/example/proyecto_tarjeta/Forget_Password.java
@@ -1,5 +1,6 @@
 package com.example.proyecto_tarjeta;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,6 +11,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
@@ -32,7 +48,7 @@ public class Forget_Password extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (!correo.getText().toString().isEmpty()) {
-
+                    generarContrasena("https://nizi.red-utz.com/solicitar_contrasena.php");
                 }else{
                     new SweetAlertDialog(Forget_Password.this, SweetAlertDialog.ERROR_TYPE)
                             .setTitleText("El correo electrónico no puede estar vacío")
@@ -49,5 +65,45 @@ public class Forget_Password extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void generarContrasena(String URL){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String respuesta = jsonObject.getString("message");
+                    if (respuesta.equals("Error correo")){
+                        new SweetAlertDialog(Forget_Password.this, SweetAlertDialog.ERROR_TYPE)
+                                .setTitleText("No existe una cuenta asociada al correo electrónico ingresado")
+                                .setConfirmButtonBackgroundColor(Color.parseColor("#100DE5"))
+                                .show();
+                    }else if (respuesta.equals("Correcto")){
+                        new SweetAlertDialog(Forget_Password.this, SweetAlertDialog.SUCCESS_TYPE)
+                                .setTitleText("Hemos enviado una contraseña temporal al correo electrónico que ingresaste")
+                                .setConfirmButtonBackgroundColor(Color.parseColor("#100DE5"))
+                                .show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error de conexión", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("correo", correo.getText().toString());
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
